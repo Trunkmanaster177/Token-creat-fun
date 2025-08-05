d2da391765536db943b1fab98f8478b0114b84c0
Trunkmanaster177 <newmusic177@gmail.com>
2025-08-05 11:02:37 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
+import React from 'react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import CreateToken from './components/CreateToken';
 
-import React from "react";
-import { WalletProvider, ConnectionProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter
-} from "@solana/wallet-adapter-wallets";
-
-import '@solana/wallet-adapter-react-ui/styles.css';
-
-const App = () => {
-  const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
-
+function App() {
   return (
-    <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <div style={{ padding: 40 }}>
-            <h1>Create Token dApp</h1>
-            <WalletMultiButton />
-            <p>More features like token creation, liquidity tools, etc. coming...</p>
-          </div>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Create Your SPL Token</h1>
+      <WalletMultiButton />
+      <CreateToken />
+    </div>
   );
-};
+}
 
 export default App;
