c0eff48bdfc680dfc3a07a348d7a118fb4e98bee
Trunkmanaster177 <newmusic177@gmail.com>
2025-08-05 11:03:21 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import {
+  WalletModalProvider
+} from '@solana/wallet-adapter-react-ui';
+
+import {
+  PhantomWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const network = WalletAdapterNetwork.Devnet;
+const endpoint = 'https://api.devnet.solana.com';
+const wallets = [new PhantomWalletAdapter()];
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ConnectionProvider endpoint={endpoint}>
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
+);
