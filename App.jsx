3ed1ae7e9af5653f56fa886672e822ea0cc69511
Trunkmanaster177 <newmusic177@gmail.com>
2025-08-05 09:57:00 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', backgroundColor: '#111', color: '#fff', minHeight: '100vh' }}>
+      <h1>Create Token on Solana</h1>
+      <p>🚀 Wallet connect + token creation will be integrated here</p>
+    </div>
+  )
+}
+
+export default App
