a72ead03bfbd4bd64a88730eff8150214f92d9a3
Trunkmanaster177 <newmusic177@gmail.com>
2025-08-05 10:23:49 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,30 @@
 
-import React from 'react'
+import React from "react";
+import { WalletProvider, ConnectionProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter
+} from "@solana/wallet-adapter-wallets";
 
-function App() {
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const App = () => {
+  const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+
   return (
-    <div style={{ padding: '20px', backgroundColor: '#111', color: '#fff', minHeight: '100vh' }}>
-      <h1>Create Token on Solana</h1>
-      <p>🚀 Wallet connect + token creation will be integrated here</p>
-    </div>
-  )
-}
+    <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <div style={{ padding: 40 }}>
+            <h1>Create Token dApp</h1>
+            <WalletMultiButton />
+            <p>More features like token creation, liquidity tools, etc. coming...</p>
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
 
-export default App
+export default App;
